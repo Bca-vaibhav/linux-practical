fe5deff398f524b8c605af21c86331e1b9aa2aaa
Bca-vaibhav <bcawithvaibhav@gmail.com>
2024-06-05 08:47:29 +0530

    Add files via upload

---
+++ practical 6.cpp
@@ -0,0 +1,182 @@
+List the names of the files according to their last access time along with their creation date and time. Also give the comma? t? extract the links, file owner and the file name only in the current directory/
+
+pubuntu@pubuntu:~$ ls -ul | tr -s" I cut -d-16-9
+
+2015-03-13 03:04 ashish
+
+2015-03-13 02:58 b4
+
+2015-03-17 06:18 bcal
+
+2015-02-23 03:03 books.txt
+
+2015-03-10 02:44 cal
+
+2009-09-06 10:59 Desktop
+
+2015-03-13 03:33 dir
+
+2015-03-13 03:38 dirl
+
+pubuntu@pubuntu:~$ ls -ul | trs ""| cut -d "" -f2,3,4,8
+
+172
+
+2 pubuntupubuntuashish
+
+2 pubuntupubuntu b4
+
+10 pubuntupubuntu bcal
+
+1 pubuntupubuntu books.txt
+
+1 pubuntupubuntucal
+
+2 pubuntupubuntu Desktop
+
+1 pubuntupubuntudir
+
+1 pubuntupubuntu dirl
+
+1 pubuntupubuntuexamples.desktop
+
+I pubuntupubuntu gl
+
+1 pubuntupubuntu girls.txt
+
+2 pubuntupubuntu hello
+
+//output//
+pubuntu@pubuntu:~$ ls -ul | tr -s " " | cut -d " " -f6-9
+
+2015-03-13
+
+03:04
+
+ashish
+
+2015-03-13
+
+02:58 b4
+
+2015-03-17 06:18
+
+bcal
+
+2015-02-23
+
+03:03 books.txt
+
+2015-03-10
+
+02:44
+
+cal
+
+2009-09-06
+
+10:59 Desktop 03:33 dir
+
+2015-03-13
+
+2015-03-13
+
+03:38
+
+dirl
+
+2009-09-06
+
+13:01
+
+examples.desktop
+
+2015-02-13
+
+05:44 gl
+
+2015-03-03
+
+03:16
+
+girls.txt
+
+2015-03-13
+
+03:04 hello
+
+2015-03-17
+
+01:11 himanshu.txt
+
+2015-03-13
+
+03:04 neetu
+
+2015-03-13
+
+03:04 neha
+
+2015-03-13
+
+03:04 nitu
+
+2015-02-13
+
+03:38 nitul
+
+2015-02-13
+
+03:01 nitul2
+
+2015-03-13
+
+03:04 nitu2
+
+2015-03-13
+
+03:04 nitu3
+
+2015-03-13
+
+03:04 nitu4
+
+putrunto@pubuntu:~$ ls -ul | 172 tr -s " " | cut -d " "
+ -f 2,3,4,8
+
+2 pubuntu pubuntu ashish
+
+2 pubuntu pubuntu b4
+
+10 pubuntu pubuntu bral 1 pubuntu pubuntu books.txt
+
+1 pubuntu pubuntu cal
+
+2 pubuntu pubuntu Desktop pubuntu pubuntu dir
+
+1 1 pubuntu pubuntu dirl
+
+pubuntu pubuntu examples.desktop
+
+1 1 pubuntu pubuntu gl
+
+1 pubuntu pubuntu girls.txt
+
+2 pubuntu pubuntu hello
+
+1 pubuntu pubuntu himanshu.txt
+
+2 pubuntu pubuntu neetu pubuntu pubuntu neha
+
+2 2 pubuntu pubuntu nitu
+
+3 pubuntu pubuntu nitul
+
+2 pubuntu pubuntu nitu12
+
+2 pubuntu pubuntu nitu2 pubuntu pubuntu nitu3
+
+3 2 pubuntu pubuntu nitu4
+
+2 pubuntu pubuntu nitus
+
