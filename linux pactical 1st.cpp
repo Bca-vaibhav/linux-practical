5790cf878b76797390cdfeb66f0808e38701392c
Bca-vaibhav <bcawithvaibhav@gmail.com>
2024-06-05 08:00:52 +0530

    Add files via upload

---
+++ linux pactical 1st.cpp
@@ -0,0 +1,66 @@
+irectory Verify have actually stud stud you changed to directory Return to your original directory.Also create a file called top. Display the first three and last three lines of the file top in the same directory.
+
+pubuntu@pubuntu:~$ cd rubina
+
+pubuntu@pubuntu:~/rubinas mkdir stud
+
+pubuntu@pubuntu:~/rubinas cd stud
+
+pubuntu@pubuntu:~/rubina/stud$ /home/pubuntu/rubina/stud pwd
+
+pubuntu@pubuntu:~/rubina/stud$ cd
+
+pubuntu@pubuntu:~/rubinas
+
+pubuntu@pubuntu:~/rubinas cat > top//Creation of file named Top Hi this is linux programming.
+
+Class BCA I (II Sem). Department Of Computer Science.
+
+D.B.Science College.
+
+Gondia (Maharashtra).
+
+pubuntu@pubuntu:~/rubinas head -3 top
+
+Hi this is linux programming.
+
+Class BCA I (II Sem).
+
+Department Of Computer Science.
+
+pubuntu@pubuntu:~/rubinas tail -3 top Department Of Computer Science.
+
+D.B.Science College.
+
+Gondia (Maharashtra).
+
+
+  //output//
+  pubuntu@pubuntu:~/rubinas cat > top Hi this is linux programming.
+
+Class BCA I (II Semi. Department of Computer Science.
+
+D.B.Science College.
+
+Gondia (Maharashtra).
+
+pubuntu@pubuntu:~/rubinas head -3 top Hi this is Linux programming.
+
+Class BCA I (II Sem).
+
+Department of Computer Science.
+
+pubuntu@pubuntu:~/rubinas tail 3 top Department of Computer Science.
+
+D.B.Science College. Gondia (Maharashtra).
+
+pubuntu@pubuntu:~/rubinas
+pubuntu@pubuntu:~$ cd rubina
+
+pubuntu@pubuntu:~/rubinas mkdir stud
+
+pubuntu@pubuntu:/rubina$ cd stud pubuntu@pubuntu:/rubina/studs pwd
+
+/home/pubuntu/rubina/stud pubuntu@pubuntu:~/rubina/stud$ cd ..
+
+pubuntu@pubuntu:~/rubinas 
