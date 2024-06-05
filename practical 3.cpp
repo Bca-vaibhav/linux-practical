66448e3c030f83b4d152a6df02c2ac74f29ba9c1
Bca-vaibhav <bcawithvaibhav@gmail.com>
2024-06-05 08:23:37 +0530

    Add files via upload

---
+++ practical 3.cpp
@@ -0,0 +1,137 @@
+create a directory called target move ant two files to drectory 
+target . display the content of the above mention file remove the file list 
+the content s of the direcotry and change to home direcotry
+
+pubuntu@pubuntu:~/rubinas mkdir target
+
+pubuntu@pubuntu:/rubinas mv/rubina/stud/shikhal target pubuntu@pubuntu:/rubinas cat ~/rubina/target/shikhal
+
+hjfjgj
+
+hhjgjg
+
+hgjkgjkgjk
+
+uyuiryiiortyui
+
+pubuntu@pubuntu:~/rubinas
+
+pubuntu@pubuntu:~/rubinas rm/rubina/target/shikhal
+
+pubuntu@pubuntu:~/rubinas cd target
+
+pubuntu@pubuntu:~/rubina/targets is
+
+pubuntu@pubuntu:~/rubina/targets 1s
+
+pubuntu@pubuntu:~/rubina/target$ cd
+
+pubuntu@pubuntu:~/rubinas
+
+
+
+output
+
+pubuntu@pubuntu:~/rubinas mkdir target 
+pubuntu@pubuntu:-/rubinas mv -/rubina/stud/shikhal target
+
+pubuntu@pubuntu:/rubinas pubuntu@pubuntu:/rubinas
+pubuntu@pubuntu:-/rubinas cat -/rubina/target/shikhal
+
+hjfjgj
+
+hhjgjg
+
+hgjkgjkgjk
+
+uyuiryiiortyui
+
+pubuntu@pubuntu:~/rubinas
+
+pubuntu@pubuntu:/rubinas rm-/rubina/target/shikhal 
+pubuntu@pubuntu:/rubinas cd target
+
+pubuntu@pubuntu:/rubina/targets is 
+pubuntu@pubuntu:/rubina/targets ls
+
+pubuntu@pubuntu:~/rubina/targets cd
+
+pubuntu@pubuntu:/rubinas pubuntu@pubuntu:/rubinas
+
+pubuntu@pubuntu:$ ls -p :$ ls -p
+
+pubuntu@pubuntu
+
+ash Esh/
+
+DEBL
+
+books.EXE
+
+dir darl
+
+dirl
+
+neetu/
+
+natu3/ nitus/
+
+neha/ nitu/
+
+rohit rubina/
+
+nitus/
+
+nitu12/
+
+personal/
+
+nituz/
+
+pracht/
+
+purnima
+
+shikha/
+
+target3/
+
+shubham/top
+
+stud studi/
+
+target/
+
+topp
+
+arvi
+
+examples.desktop
+
+01
+
+girls.txt
+
+pubuntu@pubuntu:-s head dirl
+
+shil
+
+ashish/
+
+64/
+
+bcal/
+
+books.txt
+
+cal Desktop/
+
+dir
+
+dirt
+
+examples.desktop
+
+91
+
+pubuntu@ubuntu:-S
