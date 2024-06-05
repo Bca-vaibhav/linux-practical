fc989a2d731fa149d4df85a9ab58b15ea40bab3b
Bca-vaibhav <bcawithvaibhav@gmail.com>
2024-06-05 08:14:40 +0530

    Add files via upload

---
+++ linux practical 2.cpp
@@ -0,0 +1,76 @@
+// change to stud directory create a direactory under directory called marsk
+ copy any two line from home directory to marks delete the 
+directory marks using ioptino cahnge o hmoe directory copy any two files 
+from current direcotry to stude directory create addiotional names for them
+pubuntu@pubuntu:~/rubinas cd stud
+
+pubuntu@pubuntu:~/rubinas cd stud
+
+pubuntu@pubuntu:~/rubina/studs mkdir marks
+
+pubuntu@pubuntu:~/rubina/stud$ cp/rubina/shikha marka
+
+pubuntu@pubuntu:~/rubina/stud$ cp/rubina/rubina marks
+
+pubuntu@pubuntu:-/rubina/stud$ rm-ir marks
+
+rm: descend into directory marks'? y
+
+rm: remove regular file marks/shikha? y
+
+zm: remove regular file marks/rubina'? y
+
+im: remove directory "marks'? y
+
+pubuntu@pubuntu:~/rubina/studs
+
+pubuntu@pubuntu:~/rubina/stud$ cd ..//Change to home directory
+
+pubuntu@pubuntu:~/rubinas epshikha ~/rubina/stud
+
+pubuntu@pubuntu:~/rubinas cprubina/rubina/stud
+
+pubuntu@pubuntu:-/rubinas In-/rubina/stud/rubina/stud/shikhal In: /home/pubuntu/rubina/stud': hard link not allowed for
+
+directory
+
+pubuntu@pubuntu:~/rubinas In-/rubina/stud/shikha -/rubina/stud/shikhal
+
+pubuntu@pubuntu:~/rubinas in ~/rubina/stud/rubina
+
+-/rubina/stud/rubinal
+
+  //output//
+
+pubuntu@pubuntu:-/rubinas cd stud
+
+pubuntuloubuntu:-/rubina/studs ekdir marks puburtuspubuntu:-/rubina/studs cp rubina/shikha marks
+
+ca cannot stat rubina/shikha: No such file or directory pubuntu pubuntur-/rubina/studs cp/rubina/shikha marks
+
+pubuntu@pubuntur/rubina/studs cp/rubina/rutina marks subuntu ubuntu:-/robina/studs rm-ir marks
+
+descend into directory marks pubuntu@pubuntu-/rubina/studs cd...
+
+pubuntu@pubuntu:-/rubinas co stud pubuntu@pubuntu:-/rubina/studs La
+
+topl
+
+Pubuntuuntur/rubina/studs có marks
+
+ubuntu-buntu/rubina/stud/markss ed pubunturigubuntu/rubina/studs rm ir marks
+
+Descend into directory marks 7 y
+
+re: remove repular file marks/shikha' y re remove regular file marks/rubina'? y
+
+remove directory tory marks' y pubuntu@pubuntur-/rubina/studs
+Practical Recere
+
+Applications Places System Sat Fe
+
+pubuntu@pubuntu:/rubina/studs cd.. pubuntu@pubuntu:/rubinas cp shikha-/rubina/stud pubuntu@pubuntu:~/rubinas cp rubina /rubina/stud
+
+pubun/home/pubuntu/rubina/stud hard studnet/rubina/stud/shikhal pubuntu@pubuntu:~/rubinas In pubuntu@pubuntu:-/rubinas In -/rubina/stud/shikhaowedrubina/stud/shikha -/rubina/stud/rubina shikhal -/rubina/stud/rubinal
+
+pubuntu@pubuntu:~/rubinas
